1c4634b80d457bdab01a4c1e9c6fe90bf3aa4beb
mora avalos <moavalos@alumno.unlam.edu.ar>
2022-06-15 15:52:12 -0300

    test creaciones correctas

--- src/dominio/Vehiculo.java
+++ src/dominio/Vehiculo.java
@@ -9,19 +9,40 @@ public class Vehiculo {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(latitud, longitud);
+		// return Objects.hash(latitud, longitud);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((latitud == null) ? 0 : latitud.hashCode());
+		result = prime * result + ((longitud == null) ? 0 : longitud.hashCode());
+		return result;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
+//		if (this == obj)
+//			return true;
+//		if (obj == null)
+//			return false;
+//		if (getClass() != obj.getClass())
+//			return false;
+//		Vehiculo other = (Vehiculo) obj;
+//		return Objects.equals(latitud, other.latitud) && Objects.equals(longitud, other.longitud);
 		if (this == obj)
 			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
+		if (!(obj instanceof Vehiculo))
 			return false;
 		Vehiculo other = (Vehiculo) obj;
-		return Objects.equals(latitud, other.latitud) && Objects.equals(longitud, other.longitud);
+		if (latitud == null) {
+			if (other.latitud != null)
+				return false;
+		} else if (!latitud.equals(other.latitud))
+			return false;
+		if (longitud == null) {
+			if (other.longitud != null)
+				return false;
+		} else if (!longitud.equals(other.longitud))
+			return false;
+		return true;
 	}
 
 	public Double getLongitud() {
--- test/interfaz/TestSistema.java
+++ test/interfaz/TestSistema.java
@@ -5,9 +5,12 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import dominio.Auto;
+import dominio.Bicicleta;
+import dominio.Moto;
+import dominio.Tren;
 
 public class TestSistema {
-	
+
 	@Test
 	public void queSePuedaCrearUnAuto() {
 		Auto nuevo = new Auto("JJZ526", 5, 240, 41.40338, 2.17403);
@@ -16,6 +19,35 @@ public class TestSistema {
 		assertEquals((Integer) 240, nuevo.getVelocidadMaxima());
 		assertEquals((Double) 41.40338, nuevo.getLatitud());
 		assertEquals((Double) 2.17403, nuevo.getLongitud());
+	}
+
+	@Test
+	public void queSePuedaCrearUnaMoto() {
+		Moto nuevo = new Moto("094AB5", 200, 41.40338, 2.17403);
+		assertEquals("094AB5", nuevo.getPatente());
+		assertEquals((Integer) 2, nuevo.getCANTIDAD_MAXIMA_DE_PASAJEROS());
+		assertEquals((Integer) 200, nuevo.getVelocidadMaxima());
+		assertEquals((Double) 41.40338, nuevo.getLatitud());
+		assertEquals((Double) 2.17403, nuevo.getLongitud());
+	}
+
+	@Test
+	public void queSePuedaCrearUnaBicicleta() {
+		Bicicleta nuevo = new Bicicleta(41.40338, 2.17403);
+		assertEquals((Double) 41.40338, nuevo.getLatitud());
+		assertEquals((Double) 2.17403, nuevo.getLongitud());
+
+	}
+
+	@Test
+	public void queSePuedaCrearUnTren() {
+		Tren nuevo = new Tren(15, 250, 100, 41.40338, 2.17403);
+		assertEquals((Integer) 15, nuevo.getCantidadDeVagones());
+		assertEquals((Integer) 250, nuevo.getCantidadDePasajerosPorVagon());
+		assertEquals((Integer) 100, nuevo.getVelocidadMaxima());
+		assertEquals((Double) 41.40338, nuevo.getLatitud());
+		assertEquals((Double) 2.17403, nuevo.getLongitud());
+
 	}
 
 }
